50f96b1f5ac392986f054a0479c5b56b98b58559
Dean_F <stone.feng1990@gmail.com>
2019-07-14 22:27:26 +0800

    create

---
+++ Example/Pods/Target Support Files/DRProgressMaskView/DRProgressMaskView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_DRProgressMaskView : NSObject
+@end
+@implementation PodsDummy_DRProgressMaskView
+@end
---
+++ Example/Pods/Target Support Files/Pods-DRProgressMaskView_Tests/Pods-DRProgressMaskView_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DRProgressMaskView_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_DRProgressMaskView_Tests
+@end
